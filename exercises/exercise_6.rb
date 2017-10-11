0241bab23a35b4090c5691318e6c35564c8ac8bc
llcoolj84 <llcj84@gmail.com>
2017-10-11 13:38:31 +0000

    commit all exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Dwayne", last_name: "Johnson", hourly_rate: 100)
+
+@store1.employees.create(first_name: "Jennifer", last_name: "Lopez", hourly_rate: 200)
+
+@store2.employees.create(first_name: "Jason", last_name: "Statham", hourly_rate: 40)
+
+@store2.employees.create(first_name: "Johnathan", last_name: "Jefferies", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Kanye", last_name: "West", hourly_rate: 75)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter a store name:"
+
+
+@storeX = gets.chomp            # create new instance variable 
+
+new_store = Store.new(name: @storeX, annual_revenue: 500000, mens_apparel: true, womens_apparel: false)
+
+new_store.save # on attempt to save, display error message.  (NOT WORKING!!)
+
+new_store.errors.messages.each do |message| 
+  puts message
+end
